dcd20fed0516fe37b77aeba509e6ddf772e9afda
KingVictoria <TheRealKingVictoria@gmail.com>
2017-10-09 09:52:14 -0400

    First commit of revised version

---
+++ src/main/VassalCraft.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VassalCraft extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+
+}
