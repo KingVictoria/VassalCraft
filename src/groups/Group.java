09fe528a6f6ddfac681cb801a10b11cb9ea3cf17
KingVictoria <TheRealKingVictoria@gmail.com>
2017-10-09 10:24:03 -0400

    fixin it up

--- src/groups/Group.java
+++ src/groups/Group.java
@@ -1,8 +1,14 @@
 package groups;
 
+import java.util.ArrayList;
+
+import players.VPlayer;
+
 public class Group {
 	
 	private int id;
+	
+	private ArrayList<VPlayer> members;
 	
 	public Group(){
 		id = Groups.getFreeId();
@@ -11,6 +17,18 @@ public class Group {
 	
 	public int getId(){
 		return id;
+	}
+	
+	public ArrayList<VPlayer> getMembers(){
+		return members;
+	}
+	
+	public boolean hasMember(VPlayer player){
+		for(VPlayer member: members)
+			if(member.equals(player))
+				return true;
+		
+		return false;
 	}
 
 }
